c0d9c073f9f0f55e7fcb861b0d1b04fbcfd1a63b
Ibarra11 <alanjibarradev@gmail.com>
2024-02-22 21:06:27 -0800

    try again

--- client/src/components/Auth/checkAuthLoader.js
+++ client/src/components/Auth/checkAuthLoader.js
@@ -1,7 +1,13 @@
 import { redirect } from "react-router-dom";
 
 export async function checkAuth() {
-  const res = await fetch(`${import.meta.env.VITE_API_URL}/auth/me`);
+  const res = await fetch(`${import.meta.env.VITE_API_URL}/auth/me`, {
+    credentials: "include",
+    headers: {
+      "Access-Control-Allow-Origin":
+        "https://api-practice-alan-ibarra.vercel.app",
+    },
+  });
   if (res.ok) {
     return true;
   }
